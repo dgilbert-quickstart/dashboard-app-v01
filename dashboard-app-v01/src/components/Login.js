788b7f076be5961ff820c350cffdc9ca3900dc0a
dgilbert-quickstart <dgilbert.quickstart@gmail.com>
2025-04-11 20:30:56 +0100

    update react dashboard app v1

--- dashboard-app-v01/src/components/Login.js
+++ dashboard-app-v01/src/components/Login.js
@@ -1,16 +1,34 @@
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
 
 function Login()
 {
+    const txtusername = useRef("")
+    const txtpassword = useRef("")
+    const [msgText, setMsg] = useState("")
+
+    const handleLogin = (e)=>{
+
+        e.preventDefault()
+
+        console.log("# handleLogin")
+        
+        //validae field
+
+        //fetch api request 
+    }
+
     return (
         <>
             <p>Login</p>
             <p></p>
             <form>
-                <label>UserName: </label><input type="text" placeholder="* username" maxLength={25}></input><br/>
-                <label>Password: </label><input type="password" placeholder="* password" maxLength={25}></input>
+                <label>UserName: </label><input ref={txtusername} type="text" placeholder="* username" maxLength={25}></input><br/>
+                <label>Password: </label><input ref={txtpassword} type="password" placeholder="* password" maxLength={25}></input>
                 <p></p>
-                <button>Submit</button>
+                <p>{msgText}</p>
+                <p></p>
+                <button onClick={(e)=>handleLogin(e)}>Submit</button>
             </form>
             <p></p>
             <Link to="/register">Register</Link>
